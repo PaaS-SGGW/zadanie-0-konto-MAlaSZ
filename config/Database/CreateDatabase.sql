ae619937597327ae5a54482c2777fef19484aa21
Maciej Małaszek <malasz95@gmail.com>
2018-11-11 17:59:41 +0100

    Updated composer & CreateDB

--- config/Database/CreateDatabase.sql
+++ config/Database/CreateDatabase.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Users` (
   `Login` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
   `Password` VARCHAR(4097) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
   `Salt` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
-  'Language' CHAR(5) NOT NULL,
+  `Language` CHAR(5) NOT NULL,
   PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 
@@ -15,7 +15,7 @@ CREATE TABLE `Posts` (
   `UserId` INT NOT NULL ,
   `Content` LONGTEXT NOT NULL ,
   `Summary` TEXT NOT NULL ,
-  'Language' CHAR(5) NOT NULL,
+  `Language` CHAR(5) NOT NULL,
   `ImageId` INT NOT NULL ,
   PRIMARY KEY (`Id`))
   ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_unicode_ci;
@@ -37,8 +37,6 @@ CREATE TABLE `images` (
   PRIMARY KEY (`Id`))
   ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_unicode_ci;
 
-  Insert INTO constantposts values(1,'pl-PL','1','Testowy wpis','Treść testowego wpisu','1')
-  UPDATE images Set Path='forest.jpg'
-
-  INSERT INTO images VALUES(1,'forest','forest.jpg');
-  INSERT INTO Users VALUES(1,'Administrator','admin','740df87d92ef250275508e72b335a2760bf42704d33933bba08183622522a09c14558ea17e2cc9a7874a9524a20f0b5ad878feb37debfb8f616013d31b7c95f8','ujfR1mTsFpSyULnCL8CXIh2TdQe0451O','pl-PL');  
+INSERT INTO constantposts values(1,'pl-PL','1','Testowy wpis','Treść testowego wpisu','1');
+INSERT INTO images VALUES(1,'forest','forest.jpg');
+INSERT INTO Users VALUES(1,'Administrator','admin','740df87d92ef250275508e72b335a2760bf42704d33933bba08183622522a09c14558ea17e2cc9a7874a9524a20f0b5ad878feb37debfb8f616013d31b7c95f8','ujfR1mTsFpSyULnCL8CXIh2TdQe0451O','pl-PL');  
